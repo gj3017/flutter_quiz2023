65697241db3d4582fcbf9826a08ea9ac9c44166e
gj3017 <gj3017@163.com>
2023-05-30 23:17:46 +0800

    添加学习注释

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -24,6 +24,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
   void answerQuestion(String selectedAnswer) {
     //在有状态组件的Widget类里获取父组件地址后，通过State类里的widget直接调用
     //这里的widget是内置用法，类似于this
+    //在子组件里调用函数，传入答题者的答案，真正函数运行是在父组件里定义好的
     widget.onSelectAnswer(selectedAnswer);
     setState(() {
       currentQuestionIndex++; // increments the value by 1
@@ -52,8 +53,8 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
-            //这是一个常用写法，通过扩展操作符和map函数将List列表里的元素用小组件展现出来
-            //使用扩展操作符（...）将一个 List 中的所有元素插入到另一个 List 中
+            //这是一个常用写法，通过扩展操作符和map函数将Iterable列表里的元素用小组件展现出来
+            //使用扩展操作符（...）将一个 List 中的所有元素插入到另一个 List 中，比如将[3,4]插入[1,2],不用扩展操作符就变成[1,2,[3,4]],使用扩展操作符插入后就是[1,2,3,4]
             ...currentQuestion.shuffledAnswers.map((answer) {
               return AnswerButton(
                 answerText: answer,
